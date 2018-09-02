b77738103dac4bc9785f19e03925766e6ea4bce7
Sasha Bazilski <sasha.bazilski@gmail.com>
2018-09-02 13:25:11 +0300

    add hw-2-1

---
+++ js/hw-2-1/js/index.js
@@ -0,0 +1,16 @@
+'use strict';
+let userInput;
+let total = 0;
+const numbers = [];
+
+do {
+  userInput = prompt('Введите число');
+  numbers.push(userInput);
+} while (userInput !== null);
+numbers.pop();
+for (const i of numbers) {
+  total += Number(i);
+}
+if (numbers.length > 0) {
+  alert(`Общая сумма чисел равна ${total}`);
+} else alert('Массив  пустой');
