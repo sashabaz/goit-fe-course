509e28fea5b1048fe9ca77d702b030fcdd8db874
Sasha Bazilski <sasha.bazilski@gmail.com>
2018-09-06 12:48:38 +0300

    add

--- js/hw-2-1/js/index.js
+++ js/hw-2-1/js/index.js
@@ -3,13 +3,21 @@ let userInput;
 let total = 0;
 const numbers = [];
 do {
-  userInput = prompt('Введите число');
-  numbers.push(userInput);
+  userInput = prompt('Введите число.');
+  if (userInput !== null && !isNaN(userInput)) {
+    numbers.push(userInput);
+  }
+  if (isNaN(userInput)) {
+    alert('Было введено не число, попробуйте еще раз.');
+  }
 } while (userInput !== null);
 
-if (numbers.length > 0) {
+if (numbers.length == 0) {
+  alert('Пуст');
+} else {
   for (const i of numbers) {
     total += Number(i);
   }
-  alert(`Общая сумма чисел равна ${total}`);
+  alert(`Общая сумма чисел равна ${total}.`);
 }
+
