ced6cd97ba1dedb647b4291eb8ee0b66ecfa5a9d
Sasha Bazilski <sasha.bazilski@gmail.com>
2018-09-06 10:24:33 +0300

    add up date

--- js/hw-2-1/js/index.js
+++ js/hw-2-1/js/index.js
@@ -2,17 +2,14 @@
 let userInput;
 let total = 0;
 const numbers = [];
-
 do {
   userInput = prompt('Введите число');
   numbers.push(userInput);
 } while (userInput !== null);
-numbers.pop();
-for (const i of numbers) {
-  total += Number(i);
-}
+
 if (numbers.length > 0) {
+  for (const i of numbers) {
+    total += Number(i);
+  }
   alert(`Общая сумма чисел равна ${total}`);
-} else {
-  alert('Массив  пустой');
 }
