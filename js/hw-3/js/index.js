fbaa7f350089448aa0faad220e360418faf74108
Sasha Bazilski <sasha.bazilski@gmail.com>
2018-09-10 20:41:59 +0300

    add

--- js/hw-3/js/index.js
+++ js/hw-3/js/index.js
@@ -1,19 +1,19 @@
 'use strict';
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-const isLoginValid = function (login) {
+const isLoginValid = function(login) {
   const min = 4;
   const max = 16;
   const isLogin = login.length >= min && login.length <= max;
   return isLogin;
 };
 
-const isLoginUnique = function (allLogins, login) {
-  const isUnique = !logins.includes(login);
+const isLoginUnique = function(allLogins, login) {
+  const isUnique = !allLogins.includes(login);
   return isUnique;
 };
 
-const addLogin = function (login) {
+const addLogin = function(login) {
   const isLogin = isLoginValid(login);
   const loginIsUnique = isLoginUnique(logins, login);
   if (!isLogin) {
