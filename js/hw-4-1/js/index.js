5eca173b6560a5e9adb0edabd6628bb7504695da
Sasha Bazilski <sasha.bazilski@gmail.com>
2018-09-27 06:34:22 +0300

    add hw 4

---
+++ js/hw-4-1/js/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const products = {
+  bread: 10,
+  milk: 15,
+  apples: 20,
+  chicken: 50,
+  cheese: 40,
+};
+
+function Cashier(name, productDatabase) {
+  this.name = name;
+  this.productDatabase = productDatabase;
+  this.customerMoney = 0;
+  this.getCustomerMoney = (value) => {
+    this.customerMoney = value;
+  };
+  this.countTotalPrice = (order) => {
+    let sum = 0;
+    for (key in order) {
+      sum += order[key] * this.productDatabase[key];
+    }
+    return sum;
+  };
+
+  this.countChange = totalPrice => (this.customerMoney > totalPrice ? this.customerMoney - totalPrice : null);
+
+  this.onSuccess = (change) => {
+    console.log(`Спасибо за покупку, ваша сдача ${change}!`);
+  };
+  this.onError = () => {
+    console.log('Очень жаль, вам не хватает денег на покупки');
+  };
+  this.reset = () => {
+    this.customerMoney = 0;
+  };
+}
+
+const order = {
+  bread: 2,
+  milk: 2,
+  apples: 1,
+  cheese: 1,
+};
+
+const mango = new Cashier('Mango', products);
+console.log(mango.name);
+console.log(mango.productDatabase);
+console.log(mango.customerMoney);
+
+const totalPrice = mango.countTotalPrice(order);
+
+console.log(totalPrice); // 110
+
+mango.getCustomerMoney(300);
+console.log(mango.customerMoney); // 300
+
+const change = mango.countChange();
+console.log(change); // 190
