6b223d36118b179dac537d712acea5fde589dfed
Sasha Bazilski <sasha.bazilski@gmail.com>
2018-10-23 21:19:06 +0300

    add hw6

---
+++ js/hw-6-1/js/index.js
@@ -0,0 +1,148 @@
+class Hamburger {
+  /**
+   * @constructor
+   * @param {String} size - Размер
+   * @param {String} stuffing - Начинка
+   */
+  constructor(size, stuffing) {
+    this._size = size;
+    this._stuffing = stuffing;
+    this._toppings = [];
+  }
+
+  /**
+   * Добавить topping к гамбургеру. Можно добавить несколько topping, при условии, что они разные.
+   * @param {String} topping - Тип добавки
+   */
+  addTopping(topping) {
+    this._toppings.includes(topping) ? this._toppings : this._toppings.push(topping);
+  }
+
+  /**
+   * Убрать topping, при условии, что она ранее была добавлена
+   * @param {String} topping - Тип добавки
+   */
+  removeTopping(topping) {
+    if (this._toppings.includes(topping)) {
+      this._toppings.splice(this._toppings.indexOf(topping), 1);
+    }
+  }
+
+  /**
+   * Получить список toppings
+   * @returns {Array} - Массив добавленных topping, содержит значения констант Hamburger.TOPPING_*
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.toppings и нам вернет массив добавок
+   */
+  get getToppings() {
+    return this._toppings;
+  }
+
+  /**
+   * Узнать размер гамбургера
+   * @returns {String} - размер гамбургера
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.size и нам вернет размер
+   */
+  get getSize() {
+    return this._size;
+  }
+
+  /**
+   * Узнать начинку гамбургера
+   * @returns {String} - начинка гамбургера
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.stuffing и нам вернет начинку
+   */
+  getStuffing() {
+    return this._stuffing;
+  }
+
+  /**
+   * Узнать цену гамбургера
+   * @returns {Number} - Цена в деньгах
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.price и нам вернет сумму.
+   */
+
+  calculatePrice() {
+    return (
+      Hamburger.SIZES[this._size].price
+      + Hamburger.STUFFINGS[this._stuffing].price
+      + this._toppings.reduce((acc, el, i) => (acc += Hamburger.TOPPINGS[this._toppings[i]].price), 0)
+    );
+  }
+
+  /**
+   * Узнать калорийность
+   * @returns {Number} - Калорийность в калориях
+   *
+   * Попробуйте сделать это геттером чтобы можно было обращаться как obj.calories и нам вернет сумму.
+   */
+  calculateCalories() {
+    return (
+      Hamburger.SIZES[this._size].calories
+      + Hamburger.STUFFINGS[this._stuffing].calories
+      + this._toppings.reduce(
+        (acc, el, i) => (acc += Hamburger.TOPPINGS[this._toppings[i]].calories),
+        0,
+      )
+    );
+  }
+}
+Hamburger.SIZE_SMALL = 'SIZE_SMALL';
+Hamburger.SIZE_LARGE = 'SIZE_LARGE';
+
+Hamburger.SIZES = {
+  [Hamburger.SIZE_SMALL]: {
+    price: 30,
+    calories: 50,
+  },
+  [Hamburger.SIZE_LARGE]: {
+    price: 50,
+    calories: 100,
+  },
+};
+
+Hamburger.STUFFING_CHEESE = 'STUFFING_CHEESE';
+Hamburger.STUFFING_SALAD = 'STUFFING_SALAD';
+Hamburger.STUFFING_MEAT = 'STUFFING_MEAT';
+
+Hamburger.STUFFINGS = {
+  [Hamburger.STUFFING_CHEESE]: {
+    price: 15,
+    calories: 20,
+  },
+  [Hamburger.STUFFING_SALAD]: {
+    price: 20,
+    calories: 5,
+  },
+  [Hamburger.STUFFING_MEAT]: {
+    price: 35,
+    calories: 15,
+  },
+};
+
+Hamburger.TOPPING_SPICE = 'TOPPING_SPICE';
+Hamburger.TOPPING_SAUCE = 'TOPPING_SAUCE';
+
+Hamburger.TOPPINGS = {
+  [Hamburger.TOPPING_SPICE]: {
+    price: 10,
+    calories: 0,
+  },
+  [Hamburger.TOPPING_SAUCE]: {
+    price: 15,
+    calories: 5,
+  },
+};
+
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+console.log('Calories: ', hamburger.calculateCalories());
+console.log('Price: ', hamburger.calculatePrice());
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+console.log('Price with sauce: ', hamburger.calculatePrice());
+console.log('Is hamburger large: ', hamburger.getSize === Hamburger.SIZE_LARGE);
+hamburger.removeTopping(Hamburger.TOPPING_SPICE);
+console.log(`Hamburger has ${hamburger.getToppings.length} toppings`, hamburger.getToppings.length);
